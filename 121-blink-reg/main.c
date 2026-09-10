ddbf02dcb574916f5aa5b5af5973781fe326cb9f
Ruslan <Galaxy6Explorer@yandex.ru>
2026-09-10 16:30:16 +0300

    Сделана п1.2.1

--- 121-blink-reg/main.c
+++ 121-blink-reg/main.c
@@ -7,9 +7,12 @@ const uint LED_PIN = 25;
 
 int main()
 {
-    volatile uint32_t* gpio_out_set = (uint32_t)* (SIO_BASE + SIO_GPIO_OUT_SET_OFFSET);
-    volatile uint32_t* gpio_out_clr = (uint32_t)* (SIO_BASE + SIO_GPIO_OUT_CLR_OFFSET);
-    const uint32_t led_mask = 1u << LED_PIN
+    volatile uint32_t* gpio_out_set = (uint32_t*) (SIO_BASE + SIO_GPIO_OUT_SET_OFFSET);
+    volatile uint32_t* gpio_out_clr = (uint32_t*) (SIO_BASE + SIO_GPIO_OUT_CLR_OFFSET);
+    const uint32_t led_mask = 1u << LED_PIN;
+
+    gpio_init();
+    gpio_set_dir(LED_PIN, GPIO_OUT)
 
     while(1)
     {
