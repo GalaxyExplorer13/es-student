05a319968a5a85876133a635ed1e3c74015ae85f
Ruslan <Galaxy6Explorer@yandex.ru>
2026-09-10 16:46:19 +0300

    Сделана п1.2.1

--- 112-blink/main.c
+++ 112-blink/main.c
@@ -1,6 +1,11 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+#define SIO_BASE 0xd00000000
+#define GPIO_OUT_SET 0x014
+#define GPIO_OUT_CLR 0x018
+
+
 const uint LED_PIN = 25;
 
 int main()
