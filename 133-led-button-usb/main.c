ef485afcb59b12601586a2a16b7f1da5e770728f
Ruslan <Galaxy6Explorer@yandex.ru>
2026-09-23 23:19:39 +0300

    Сделана п1.3.4

--- 133-led-button-usb/main.c
+++ 133-led-button-usb/main.c
@@ -19,6 +19,25 @@ void set_led(bool on)
     printf("led %s\n", on ? "on" : "off");
 }
 
+bool handle_command(int command, bool led)
+{
+    if (command == 'e')
+    {
+        led = true;
+        set_led(led);
+    }
+    else if (command == 'd')
+    {
+        led = false;
+        set_led(led);
+    }
+    else
+    {
+        printf("unknown command: %c\n", command);
+    }
+
+    return led;
+}
 
 
 int main()
@@ -38,9 +57,19 @@ int main()
     bool current = get_button_debounce(PIN);
     if (previos == true && current == false)
     {
-    led = !led;
-    set_led(led);
+        led = !led;
+        set_led(led);
     }
     previos = current;
+    
+    int command = getchar_timeout_us(0);
+    if (command == PICO_ERROR_TIMEOUT)
+        {
+            continue;
+        }
+
+    led = handle_command(command, led);
+
+
     }
 }
